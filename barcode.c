ae1de8c72b03d3a14fc273a50ae987e8c5126159
rayabharapuvyshnavi <rayabharapuvyshnavi@gmail.com>
2026-09-19 04:08:40 +0000

    done

--- barcode.c
+++ barcode.c
@@ -1,8 +1,54 @@
 #include "barcode.h"
-#include <stdio.h>
 
+static const char *digit_patterns[] = {
+    "1110010", "1100110", "1101100", "1010000", "1011100",
+    "1001110", "1000010", "1000100", "1001000", "1110100"
+};
+
+static void set_column(struct image *img, int x, int height, int white) {
+    int value = white ? 255 : 0;
+    struct pixel pixel = { value, value, value };
+
+    for (int y = 0; y < height; y++) {
+        img->pixels[y * img->width + x] = pixel;
+    }
+}
+
+static void set_pattern(struct image *img, int *x, const char *pattern,
+                        int invert) {
+    for (int i = 0; pattern[i] != '\0'; i++) {
+        int white = pattern[i] == '1';
+        if (invert) {
+            white = !white;
+        }
+        set_column(img, *x, img->height, white);
+        (*x)++;
+    }
+}
 
 struct image* barcode(char* data, int width, int height) {
+    struct image *img = malloc(sizeof(struct image));
+    img->pixels = malloc(sizeof(struct pixel) * width * height);
+    img->width = width;
+    img->height = height;
 
+    int x = 0;
+    set_pattern(img, &x, "111111111", 0);
+    set_pattern(img, &x, "010", 0);
+
+    for (int i = 0; i < 6; i++) {
+        set_pattern(img, &x, digit_patterns[data[i] - '0'], 0);
+    }
+
+    set_pattern(img, &x, "10101", 0);
+
+    for (int i = 6; i < 12; i++) {
+        set_pattern(img, &x, digit_patterns[data[i] - '0'], 1);
+    }
+
+    set_pattern(img, &x, "010", 0);
+    set_pattern(img, &x, "111111111", 0);
+
+    return img;
 }
 
--- image_calc.c
+++ image_calc.c
@@ -4,20 +4,24 @@
 #include <string.h>
 
 
-int main(int argc, char** argv){
-    // TODO: parse the arguments in argv. 
-    // You can expect argv[1] to be the digits to encode
-    // You can expect argv[2] to be the integer width
-    // You can expect argv[3] to be the integer height
-    // You can expect argv[4] to be the output filepath.
 
+int main(int argc, char** argv){
     if(argc != 5) {
         printf("Incorrect number of arguments. Expected: ./build/image_calc <barcode_value> <width> <height> <output_image_path>\n");
         return -1;
     }
 
+    char *data = argv[1];
+    int width = atoi(argv[2]);
+    int height = atoi(argv[3]);
+    char *filename = argv[4];
 
-    // TODO: call barcode
-    
-    // TODO: save result to file
+    struct image *img = barcode(data, width, height);
+
+    int result = saveimage(filename, img);
+    free(img->pixels);
+    free(img);
+    return result;
 }
+
+
